4902f01b7bec440fefdaea0a254648c705bfaea5
luc-drogrown <draganlucian18@gmail.com>
2025-11-10 16:18:55 +0200

    Initial Commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,13 @@
+module com.example.ticketsapp {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens com.example.ticketsapp to javafx.fxml;
+    exports com.example.ticketsapp;
+}
